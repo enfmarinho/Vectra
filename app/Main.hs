d233390611207eabfb677204cef03a2674836afd
enfmarinho <eduardonestormarinho228@gmail.com>
2025-10-23 14:36:38 -0300

    Improving and fixing main function

--- app/Main.hs
+++ app/Main.hs
@@ -5,20 +5,25 @@ import SymbolTable
 import System.Environment (getArgs)
 import Parser
 import Scanner
+import System.Exit (exitSuccess)
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of 
         [filename] -> do
-            -- global_symbol_table <- H.new :: IO SymbolTableType
-            -- let stack = [global_symbol_table]
+            global_symbol_table <- H.new :: IO SymbolTableType
+            let stack = [global_symbol_table]
 
-            token_list <- getTokens filename
-            print token_list
-            -- result  <- parser token_list stack
-            -- case result of
-            --     Left err -> print err
-            --     Right ans -> print ans
+            lexical_result <- getTokens filename
+            token_list <- case lexical_result of
+                Left _ -> do
+                    exitSuccess -- End execution early since a lexical error has been found
+                Right token_list -> return token_list
+            
+            sintax_result <- parser token_list stack
+            case sintax_result of
+                Left err -> print err
+                Right ans -> print ans -- TODO a positive parser result should not be printed, this is only for development 
 
         _ -> putStrLn "Missing file to interprete, pass its path as a command line argument!"
