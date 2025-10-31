8f3c95c1a6745ed8abaec9e1e4397793a9f78ed9
enfmarinho <eduardonestormarinho228@gmail.com>
2025-10-30 22:09:34 -0300

    Implementing helper functions for ParserState

--- app/SymbolTable.hs
+++ app/SymbolTable.hs
@@ -4,9 +4,13 @@ module SymbolTable
   , SymbolTableType
   , SymbolTableStackType
   , StateType
-  -- , openScope
-  -- , closeScope
-  -- , updateSymbol
+  , initParserState
+  , openScope
+  , closeScope
+  , insertSymbol
+  , insertValue
+  , updateValue
+  , consultSymbol
   ) where
 
 import qualified Data.HashTable.IO as H
@@ -14,37 +18,98 @@ import Types
 import Text.Parsec
 import Control.Monad.State.Lazy
 
--- openScope :: StateType () 
--- openScope = do
---     stack <- get
---     table <- liftIO H.new
---     put (table : stack)
---
--- closeScope :: StateType ()
--- closeScope = do
---     (_ : stack) <- get
---     put stack
+initParserState :: IO ParserState
+initParserState = do
+    globalMemory <- liftIO H.new
+    globalSymbolTable <- liftIO H.new
 
--- insertSymbol :: SymbolType -> StateType ()
--- insertSymbol (symbol_id, symbol_type) = do
---     a <- consultSymbol symbol_id
---     case a of
---         Nothing -> updateSymbol (symbol_id, symbol_type)
---         Just _ -> fail "symbol already exists"
---
--- updateSymbol :: SymbolType -> StateType ()
--- updateSymbol (symbol_id, symbol_type) = do
---     st@ParserState{..} <- getState
---     case symbolTableStack of
---         [] -> fail "No open scope"
---         (top : rest) -> do
---             liftIO $ H.insert top symbol_id symbol_type
---             putState st { symbolTableStack = top:rest}
---
--- consultSymbol :: String -> StateType (Maybe Type)
--- consultSymbol symbol = do
---     ParserState{..} <- getState
---     case symbolTableStack of
---         [] -> return Nothing
---         -- TODO this is only searching on the top SymbolTable, it should search recursively
---         ((top, _) : _) -> liftIO $ H.lookup top symbol
+    return ParserState 
+        { globalMemoryTable=globalMemory
+        , globalSymbolTable=globalSymbolTable
+        , memoryTableStack=[]
+        , symbolTableStack=[]
+        , isRunning=False
+        }
+    
+
+openScope :: Bool -> StateType () 
+openScope canAccessParentTables = do
+    st@ParserState{..} <- getState
+    newTable <- liftIO H.new -- st stands for symbol table
+    newMemory <- liftIO H.new 
+    putState st {
+        symbolTableStack = (newTable, canAccessParentTables) : symbolTableStack,
+        memoryTableStack = newMemory : memoryTableStack
+        }
+
+closeScope :: StateType ()
+closeScope = do
+    st@ParserState{..} <- getState
+
+    let newSymbolStack =
+          case symbolTableStack of
+            []        -> []  -- nothing to pop
+            (_:rest)  -> rest
+
+    let newMemoryStack =
+          case memoryTableStack of
+            []        -> []  -- nothing to pop
+            (_:rest)  -> rest
+
+    putState st
+      { symbolTableStack = newSymbolStack
+      , memoryTableStack = newMemoryStack
+      }
+
+insertSymbol :: SymbolType -> StateType ()
+insertSymbol (symbol_id, symbol_type) = do
+    st@ParserState{..} <- getState
+    case symbolTableStack of
+        [] -> do
+            liftIO $ H.insert globalSymbolTable symbol_id symbol_type
+            putState st { globalSymbolTable = globalSymbolTable }
+        (top : rest) -> do
+            let (table, b) = top
+            liftIO $ H.insert table symbol_id symbol_type
+            putState st { symbolTableStack = (table, b):rest}
+
+insertValue :: MemoryType -> StateType ()
+insertValue (symbol_id, value) = do
+    st@ParserState{..} <- getState
+    case memoryTableStack of
+        [] -> do
+            liftIO $ H.insert globalMemoryTable symbol_id value
+            putState st { globalMemoryTable = globalMemoryTable }
+        (top : rest) -> do
+            liftIO $ H.insert top symbol_id value
+            putState st { memoryTableStack = top:rest}
+
+updateValue :: MemoryType -> StateType ()
+updateValue (symbol_id, value) = do
+    ParserState{..} <- getState
+    liftIO $ update (symbol_id, value) memoryTableStack
+  where
+    update :: MemoryType -> MemoryTableStackType -> IO ()
+    update (_, _) [] = return ()
+    update (n, v) (top:rest) = do
+        found <- H.lookup top n
+        case found of
+            Just _ -> H.insert top n v
+            Nothing -> update (n, v) rest
+
+consultSymbol :: String -> StateType (Maybe Type)
+consultSymbol symbol = do
+    ParserState{..} <- getState
+    liftIO $ search symbol symbolTableStack
+  where
+    search :: String -> SymbolTableStackType -> IO (Maybe Type)
+    search _ [] = return Nothing
+    search name ((table, canAccessParent):rest) = do
+        result <- H.lookup table name
+        case result of
+            Just ty -> return (Just ty)
+            Nothing ->
+                if canAccessParent then
+                    search name rest
+                else return Nothing
+
