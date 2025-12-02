76c2f638b84b03eba65a959117beead8a8d15885
enfmarinho <eduardonestormarinho228@gmail.com>
2025-12-02 11:17:37 -0300

    Stop printing tokens

--- app/Main.hs
+++ app/Main.hs
@@ -17,8 +17,6 @@ main = do
                     exitSuccess -- End execution early since a lexical error has been found
                 Right tokenList -> return tokenList
 
-            print tokenList
-
             sintaxResult <- parser tokenList
             forM_ sintaxResult print
 
